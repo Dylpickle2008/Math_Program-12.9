8d4c482ef9e2e13ee557b853a49fbd167dedb8d5
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-12-09 13:10:06 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let l = 2;
+let w = 4;
+let r = 6; 
+
+const rectangleArea = (l * w);
+const triangleArea = (.5 * 1 * w);
+const circleArea = (3.14 * r * 2);
+
+console.log(rectangleArea);
+console.log(triangleArea);
+console.log(circleArea);
